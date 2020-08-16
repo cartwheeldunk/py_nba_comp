d3d9c94869cb1e17467142b731b4aaeb7cb7d78c
Kevin San Juan <cartwheeldunk@gmail.com>
2020-08-16 12:01:40 -0500

    list of functions and modules

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import tkinter
+
+def user_input():
+
+def bbref_route():
+
+def bbref_collect():
+
+def convert_nbaheight():
+
+def convert_nbaweight():
+
+def compare_nba():
+
+def user_output():
