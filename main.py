3e164aada698a5c44d2bfcf8c57973b1a57c9d92
Kevin San Juan <cartwheeldunk@gmail.com>
2020-08-21 16:55:31 -0500

    Skills dictionary, use bbref API, select top 25 per skill, select position

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-import requests
-import json
-import tkinter
+import json, operator
+import random
+from operator import itemgetter
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
 
-def user_input():
+userskill = "passing"
+userposition = "POINT GUARD"
+skillsdict = {
+"scoring": "points",
+"passing": "assists",
+"fouling": "fouls",
+}
+skilltrans = skillsdict[userskill]
+raw = client.players_season_totals(season_end_year=2020, output_type=OutputType.JSON)
+data = json.loads(raw)
+data.sort(key=operator.itemgetter(skilltrans), reverse=True)
+top25 = data[:25]
+output = json.dumps(top25, indent=2)
+positions = json.loads(output)
+result = []
 
-def bbref_route():
+for player in positions:
+    if userposition in player['positions']:
+        result.append(player['name'])
+        continue
+    else:
+        continue
 
-def bbref_collect():
-
-def convert_nbaheight():
-
-def convert_nbaweight():
-
-def compare_nba():
-
-def user_output():
+max = 1
+if len(result) == max:
+    print(result)
+else:
+    print(random.choice(result))
